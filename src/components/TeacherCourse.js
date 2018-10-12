0363dc44e0f8738d0648aeea937c4758b89a8d25
jensnyder <jlsnyder09@gmail.com>
2018-10-12 17:35:40 -0400

    added grid and styling to TeacherCourse

--- src/components/TeacherCourse.js
+++ src/components/TeacherCourse.js
@@ -2,15 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+const styles = {
+  paper: {
+    margin: "20px"
+  }
+};
+
 class TeacherCourse extends React.Component {
   render() {
-    return <h4>{this.props.course.name}</h4>;
+    const { course, classes } = this.props;
+    return (
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>{course.name}</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>Assignments</Paper>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state);
-  console.log(ownProps);
   let courses = state.user.person.teacher.courses;
   let courseId = parseInt(ownProps.match.params.id);
   return {
@@ -18,4 +36,5 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default withRouter(connect(mapStateToProps)(TeacherCourse));
+const styledTeacherCourse = withStyles(styles)(TeacherCourse);
+export default withRouter(connect(mapStateToProps)(styledTeacherCourse));
