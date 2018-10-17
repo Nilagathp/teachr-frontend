922b6bb0699ec2fe2219e33970cbbf524c20e00e
jensnyder <jlsnyder09@gmail.com>
2018-10-17 17:38:43 -0400

    Added filter by category to StudentCourse

--- src/components/Course/StudentCourse.js
+++ src/components/Course/StudentCourse.js
@@ -12,6 +12,10 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 // import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Divider from "@material-ui/core/Divider";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
 
 import AssignmentList from "../Assignment/AssignmentList";
 
@@ -28,29 +32,70 @@ const styles = {
   },
   button: {
     paddingLeft: "20px"
+  },
+  formControl: {
+    marginLeft: "20px",
+    marginBottom: "10px",
+    minWidth: 120
   }
 };
 
-const StudentCourse = ({ course, assignments, sections, classes }) => {
-  return (
-    <React.Fragment>
-      <Typography variant="h4" className={classes.heading}>
-        {course.name}
-        {/* <Button className={classes.button} color="primary">
+class StudentCourse extends React.Component {
+  state = {
+    category: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { course, classes } = this.props;
+    let assignments = this.props.assignments;
+    this.state.category
+      ? (assignments = assignments.filter(
+          assignment => assignment.category === this.state.category
+        ))
+      : (assignments = assignments);
+    return (
+      <React.Fragment>
+        <Typography variant="h4" className={classes.heading}>
+          {course.name}
+          {/* <Button className={classes.button} color="primary">
           Send message
         </Button> */}
-      </Typography>
-
-      <Paper className={classes.paper}>
-        <Typography variant="h4" className={classes.heading}>
-          Assignments
         </Typography>
-        <Divider />
-        <AssignmentList assignments={assignments} />
-      </Paper>
-    </React.Fragment>
-  );
-};
+
+        <Paper className={classes.paper}>
+          <Typography variant="h4" className={classes.heading}>
+            Assignments
+            <FormControl className={classes.formControl}>
+              <InputLabel shrink>Filter by category:</InputLabel>
+              <Select
+                value={this.state.category}
+                onChange={this.handleChange}
+                inputProps={{ name: "category" }}
+              >
+                <MenuItem value="" />
+                <MenuItem key={0} value={"CW"}>
+                  CW
+                </MenuItem>
+                <MenuItem key={1} value={"HW"}>
+                  HW
+                </MenuItem>
+                <MenuItem key={2} value={"TQP"}>
+                  TQP
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Typography>
+          <Divider />
+          <AssignmentList assignments={assignments} />
+        </Paper>
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   let courses = state.user.person.student.courses;
