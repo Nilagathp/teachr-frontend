f6042109c372b409c85309fc8cffac6aaf0364be
jensnyder <jlsnyder09@gmail.com>
2018-10-22 09:16:28 -0400

    Added due date to TeacherAssignmentListItem

--- src/components/Assignment/Teacher/TeacherAssignmentListItem.js
+++ src/components/Assignment/Teacher/TeacherAssignmentListItem.js
@@ -5,6 +5,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Button from "@material-ui/core/Button";
+import format from "date-fns/format";
 
 const TeacherAssignmentListItem = ({ teacher, assignment, courses }) => {
   return (
@@ -19,12 +20,16 @@ const TeacherAssignmentListItem = ({ teacher, assignment, courses }) => {
         <ListItemText
           primary={assignment.name}
           secondary={`
-              ${courses[assignment.course_id]} - ${assignment.category}`}
+              ${courses[assignment.course_id]} - ${
+            assignment.category
+          } - ${format(assignment.due_date, "M/d")}`}
         />
       ) : (
         <ListItemText
           primary={assignment.name}
-          secondary={`${assignment.category} - ${assignment.points} points`}
+          secondary={`${assignment.category} - ${
+            assignment.points
+          } points - ${format(assignment.due_date, "M/d")}`}
         />
       )}
 
